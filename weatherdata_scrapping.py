8ee7fa2cf6c0498696da1d1f8df0ed93102d6994
Isaiyamudhu <58975933+isaiyamudhugit@users.noreply.github.com>
2023-03-24 20:00:56 +0530

    Update weatherdata_scrapping.py

--- weatherdata_scrapping.py
+++ weatherdata_scrapping.py
@@ -1,7 +1,7 @@
 from requests_html import HTMLSession
 
 s = HTMLSession()
-query = 'tamilnadu'
+query = 'india'
 url = f'https://www.google.com/search?q=weather+{query}'
 r = s.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
 # print(r.html.find('title'))
